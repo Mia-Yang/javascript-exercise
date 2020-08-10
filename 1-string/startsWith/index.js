cca7ea4d7f47824f75bf8e37cf74e1722a474e70
Yang Siyu <siyuyang1995@gmail.com>
2020-08-10 21:32:30 +0800

    [yang siyu]pass the startsWith test

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const count = collection.filter(carNumber => carNumber.substr(0, 2) === '粤A').length;
+  return count;
 }
