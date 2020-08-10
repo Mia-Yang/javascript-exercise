1d5d90e15065769e4c9dafc728a147bc25ceacc5
Yang Siyu <siyuyang1995@gmail.com>
2020-08-10 23:05:06 +0800

    [yang siyu]pass the practice-3

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,12 @@
 // TODO 13: 在这里写实现代码
+export default class Student {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,11 @@
 // TODO 14: 在这里写实现代码
+export default class Worker {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    return `I am a Worker. I have a job.`;
+  }
+}
