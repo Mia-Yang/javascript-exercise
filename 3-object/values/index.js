1e63690e47f9f2aadedb80bed9efab849e02c222
Yang Siyu <siyuyang1995@gmail.com>
2020-08-10 22:47:37 +0800

    [yang siyu]pass the value test

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const count = Object.values(source); //方法返回一个给定对象自己的所有可枚举属性值的数组
+  return count.map(num => parseInt(num)).reduce((x, y) => x + y);
 }
