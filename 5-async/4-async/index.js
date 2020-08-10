40058c8faf298e2a28680ad69047c203c33705a0
Yang Siyu <siyuyang1995@gmail.com>
2020-08-11 00:52:54 +0800

    [yang siyu]compeleted the async practice

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,17 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  try {
+    const result = fetch(url, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
+    document.writeln(result.name);
+  } catch (error) {
+    console.error(error);
+  }
   // end -->
 }
 
