c1f0aba406dcc28f001ea6add754c087b4c970b0
Yang Siyu <siyuyang1995@gmail.com>
2020-08-10 21:55:32 +0800

    [yang siyu]pass the filter test

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,14 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(num => num % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const arrFilter = collection.filter((item, index, collection) => {
+    return collection.indexOf(item) === index;
+  });
+  return arrFilter;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
